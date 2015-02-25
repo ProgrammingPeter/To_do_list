a711119c600bd95ea8ab73016f55e6d04770daa1
ProgrammingPeter <peter.kamaris@yahoo.com>
2015-02-25 00:14:29 -0500

    Issue with calendar to be resolved soon
    Moved folders for calendar because it wouldn’t work. Folders included
    bootstrap, bootstrap-datepicker, and moment

---
+++ DB_connect.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "To_do";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
